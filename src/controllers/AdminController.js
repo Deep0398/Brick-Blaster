09130d5659cf2fb2f51294195ea86782a10df717
pavandwivedi <pavandwivedi35@gmail.com>
2024-03-13 18:02:34 +0530

    updated

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -3,6 +3,7 @@ import {userModel} from "../models/User.js";
 import bcrypt from 'bcrypt'
 import { generateAccessToken } from "../services/generateAccessToken.service.js";
 import { error, success } from "../utills/responseWrapper.utills.js";
+import createChallengeModel from "../models/admin.challenge.model.js"
 
 export async function adminSignupController(req,res){
     try {
@@ -44,19 +45,18 @@ export async function adminLoginController(req,res){
     return res.send(success(200,accessToken));
     
 } catch (err) {
-    return res.send(error(500,err.message));
+    return res.send(500,err.message);
 }
 
 
 }
-
 
 export async function getAllUsersController(req,res){
   try {
       const users = await userModel.find({}).populate('achievements').populate('levels');
       return res.send(success(200,users));
   } catch (err) {
-      return res.send(error(500,err.message));
+      return res.send(500,err.message);
   }
 }
 
@@ -65,6 +65,96 @@ export async function deleteAllUsers(req,res){
         await userModel.deleteMany({});
         return res.send(success(200,"all users deleted successfully"))
     } catch (err) {
-        return res.send (error(500,err.message));
+        return res.send (500,err.message);
     }
+}
+
+export async function createChallengeController(req,res){
+    const {name,description,isActive,rewards,duration,challengetype,taskamount} = req.body
+    try{
+   if(!name || !description || !rewards || !duration || !challengetype || !taskamount){
+        return res.send(404,"Insufficient Data")
+    }
+
+    const newChallenge = new createChallengeModel ({
+        name, 
+        description,
+        isActive:isActive || true,
+        rewards,
+        duration,
+        challengetype,
+        taskamount
+    })
+
+    const savedChallenge = await newChallenge.save()
+
+    return res.send(success(200,savedChallenge,"challenge created successfully",savedChallenge))
+}catch (error){
+    return res.send(500,err.message)
+}
+}
+
+export async function getChallengeController(req,res){
+    try{
+
+        const challengeDetails = await createChallengeModel.find({})
+
+        if(!challengeDetails){
+            return res.send(error(404,"challenge not found"))
+        }
+        return res.send(success(200,challengeDetails,"challenge fetched successfully",challengeDetails))
+    }catch(error){
+        return res.send(500,error.message)
+    }
+}
+
+export async function updateChallengeController(req,res){
+ 
+    const {id} = req.params
+    const {name,description,isActive,rewards,duration,challengetype,taskamount} = req.body
+    try {
+      const existingChallenge = await createChallengeModel.findById(id)
+
+      if(!existingChallenge){
+        return res.send(error(404,error.message))
+      }
+      if(name){
+        existingChallenge.name = name;
+      }
+      if (description){
+        existingChallenge.description = description;
+      }
+      if (isActive != undefined){
+        existingChallenge.isActive = isActive;
+      }
+      if (rewards){
+        existingChallenge.rewards = rewards;
+      }
+      if(duration){
+        existingChallenge.duration = duration;
+      }
+      if(taskamount){
+        existingChallenge.taskamount = taskamount;
+      }
+      if(challengetype){
+        existingChallenge.challengetype = challengetype;
+      }
+
+      const updatedChallenge = await existingChallenge.save()
+
+      return res.send(success(200,updatedChallenge,"challenge updated successfully",updatedChallenge))
+    }catch(error){
+        return res.send(error(500,err.message))
+    }
+}
+
+export async function deleteChallengeController(req,res){
+    try{
+        const {id} = req.params
+        await createChallengeModel.findByIdAndUpdate(id)
+        return res.send(success(200,"challenge deleted successfully"))
+    }catch(err){
+        return res.send(500,err.message)
+    }
+
 }
--- src/routers/AdminRouter.js
+++ src/routers/AdminRouter.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { adminLoginController, adminSignupController, deleteAllUsers, getAllUsersController } from '../controllers/AdminController.js';
+import { adminLoginController, adminSignupController, deleteAllUsers, getAllUsersController,createChallengeController,updateChallengeController,deleteChallengeController} from '../controllers/AdminController.js';
 import { checkAdminLogin } from '../middleware/middlewares.js';
 
 
@@ -10,5 +10,8 @@ adminRouter.post('/register',adminSignupController)
 adminRouter.post('/login',adminLoginController);
 adminRouter.get('/retrieveall',checkAdminLogin,getAllUsersController)
 adminRouter.delete('/delete',checkAdminLogin,deleteAllUsers);
+adminRouter.post('/createChallenge',checkAdminLogin,createChallengeController);
+adminRouter.put('/updateChallenge/:id',checkAdminLogin,updateChallengeController);
+adminRouter.delete('/deleteChallenge/:id',checkAdminLogin,deleteChallengeController);
 
 export default adminRouter;
