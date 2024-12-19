640f05bf0b730653ffa5e29514b517d7bd4d564f
Kuldeep <gwstelekuldeep@gmail.com>
2024-12-19 16:06:57 +0530

    updated

--- src/routers/UserRouter.js
+++ src/routers/UserRouter.js
@@ -10,7 +10,7 @@ const userRouter = express.Router();
 userRouter.post('/userlogin',authenticLoginController);
 userRouter.post('/guestlogin',guestLoginController);
 userRouter.post('/facebookLogin',facebookLoginController);
-userRouter.get("/get",checkUserLogin, getUserController);
+userRouter.get("/get/:id", getUserController);
 userRouter.post('/updateCoins',checkUserLogin,updateCoinController);
 userRouter.post('/updateBalls',checkUserLogin,updateBallController);
 userRouter.post('/refer',checkUserLogin,referAndEarnController);
